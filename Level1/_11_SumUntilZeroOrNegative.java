418116fcacbc0e60fecc4785dae0ea503eba3752
Navam <navamsharma142@gmail.com>
2026-01-05 14:04:37 +0530

    level one problems done

---
+++ Level1/_11_SumUntilZeroOrNegative.java
@@ -0,0 +1,18 @@
+package Level1;
+
+import java.util.Scanner;
+
+public class _11_SumUntilZeroOrNegative {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double total = 0.0;
+        while (true) {
+            double value = sc.nextDouble();
+            if (value <= 0) break;
+            total += value;
+        }
+        System.out.println(total);
+        sc.close();
+    }
+
+}
